9e97d0fe126fa5c33377b84df1cd18246eff8d57
Shreya <shreyavuppala.08@gmail.com>
2025-01-16 17:30:45 +0530

    pushed

--- src/main/java/Test/Day1.java
+++ src/main/java/Test/Day1.java
@@ -9,12 +9,15 @@ public class Day1 {
     @AfterTest
     public void lastExecution(){
         System.out.println("I will execute last");
+        
 
     }
     @Test
     public void Demo() {
 
         System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("Hiii");
         Assert.assertTrue(false);
     }
     @AfterSuite
